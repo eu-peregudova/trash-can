3ab87eb54c1c438d0041fb925cd1b97969cf928c
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-06-25 12:45:47 +0300

    feat: add navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterLink, RouterModule } from '@angular/router';
+import { NavigationComponent } from './common/components/navigation/navigation.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, NavigationComponent],
+  providers: [RouterLink],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
