27ea88429b418e0410ca4a3477ef45350278e0c8
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-06-28 15:29:20 +0300

    fix: codestyle improved

--- src/app/common/services/task.service.ts
+++ src/app/common/services/task.service.ts
@@ -2,24 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
+import { environment } from '../../../environments/enviroment';
 import { Task } from '../../models/task.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:3000/';
+  private apiUrl = `${environment.apiBaseUrl}tasks`;
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(`${this.apiUrl}tasks`).pipe(map((tasks) => tasks.map(Task.fromJSON)));
+    return this.http.get<Task[]>(this.apiUrl).pipe(map((tasks) => tasks.map(Task.fromJSON)));
   }
 
   updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task[]>(`${this.apiUrl}tasks`, task).pipe(map(Task.fromJSON));
+    return this.http.put<Task[]>(this.apiUrl, task).pipe(map(Task.fromJSON));
   }
 
   createTask(task: Task): Observable<Task> {
-    return this.http.post<Task[]>(`${this.apiUrl}tasks`, task).pipe(map(Task.fromJSON));
+    return this.http.post<Task[]>(this.apiUrl, task).pipe(map(Task.fromJSON));
   }
 }
