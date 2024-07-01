19365e39f24e003b407a000095788c8f2feee564
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-07-01 11:04:53 +0300

    fix: fix codestyle issues, add unsubscriptions

--- src/app/pages/assistant/fast-prompt/fast-prompt.component.ts
+++ src/app/pages/assistant/fast-prompt/fast-prompt.component.ts
@@ -1,32 +1,35 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { FastPrompt } from '../../../models/fastPrompt.model';
 
 @Component({
   selector: 'tc-fast-prompt',
   templateUrl: './fast-prompt.component.html',
   styleUrls: ['./fast-prompt.component.scss'],
 })
-export class FastPromptComponent implements OnInit {
-  options: {
-    shop: string;
-    oneHour: string;
-    freeDay: string;
-  };
-
-  ngOnInit(): void {
-    this.options = {
-      shop: "I'm in the grocery store right now",
-      oneHour: 'I have one hour of free time',
-      freeDay: 'I have a free day today',
-    };
-  }
-
+export class FastPromptComponent {
   @Output()
   fastPromptEvent = new EventEmitter<string>();
 
   @Output()
   typeYourselfEvent = new EventEmitter<string>();
 
-  fastPrompt(value): void {
+  options: FastPrompt[] = [
+    {
+      buttonName: 'In the grocery store',
+      prompt: "I'm in the grocery store right now, what can I do",
+    },
+    {
+      buttonName: 'One hour of free time',
+      prompt: 'I have one hour of free time, what can I do now',
+    },
+    {
+      buttonName: 'Free day',
+      prompt: 'I have a free day today',
+    },
+  ];
+
+  fastPrompt(value: string): void {
     this.fastPromptEvent.emit(value);
   }
 
