0995ab0fe6bb89df1c6fee81c0000ab011a629f7
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-07-02 16:22:47 +0300

    feat: setup component + update service

---
+++ src/app/pages/tasks/query-bar/query-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-query-bar',
+  templateUrl: './query-bar.component.html',
+  styleUrls: ['./query-bar.component.scss'],
+})
+export class QueryBarComponent {}
