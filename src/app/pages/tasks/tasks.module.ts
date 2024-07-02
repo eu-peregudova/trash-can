14a6d7314b076b552d897da2d0902266e415c5b2
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-07-02 14:16:08 +0300

    feat: implement logic division for task card

--- src/app/pages/tasks/tasks.module.ts
+++ src/app/pages/tasks/tasks.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { TaskCardComponent } from './task-card/task-card.component';
+import { TaskCardComponent } from '../../common/components/task/task-card/task-card.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 const routes: Routes = [
