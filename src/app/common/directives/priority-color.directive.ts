6b76b50abe93787b60dd1cc55bd69cee384b3c1c
Eugenia <peregoodovaeva@gmail.com>
2024-07-06 17:15:47 +0300

    feat: create directive for priority marker

---
+++ src/app/common/directives/priority-color.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
+import {TaskPriority} from "../../models/task.model";
+
+@Directive({
+  selector: '[tcPriorityColor]',
+  standalone: true,
+})
+export class PriorityColorDirective implements OnInit{
+  @Input() tcPriorityColor: string;
+  colors = {
+    [TaskPriority.Sooner]: '#E1AEAE',
+    [TaskPriority.Later]: '#FFE68E',
+    [TaskPriority. MaybeNever]: '#649869',
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', this.colors[this.tcPriorityColor]);
+  }
+}
