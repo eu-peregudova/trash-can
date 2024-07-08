aa6985d92c4ed3bf5849f442946f12fc87f16486
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-07-08 14:23:18 +0300

    fix: add variables scss

--- src/app/common/directives/priority-color.directive.ts
+++ src/app/common/directives/priority-color.directive.ts
@@ -10,7 +10,7 @@ export class PriorityColorDirective implements OnInit{
   colors = {
     [TaskPriority.Sooner]: '#E1AEAE',
     [TaskPriority.Later]: '#FFE68E',
-    [TaskPriority. MaybeNever]: '#649869',
+    [TaskPriority.MaybeNever]: '#649869',
   }
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
