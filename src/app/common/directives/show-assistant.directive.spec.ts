6447e0fb9d58d94f0400a30afcc22863fde35b2e
Evgeniia <90528280+eu-peregudova@users.noreply.github.com>
2024-07-10 17:20:29 +0300

    feat: fix delete modal mobile

--- src/app/common/directives/show-assistant.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowAssiatantDirective } from './show-assistant.directive';
-
-describe('ShowAssiatantDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowAssiatantDirective();
-    expect(directive).toBeTruthy();
-  });
-});
