e4c20b70d8ab4a8c078ad123f641a069bb79cad3
Aleksandr Shtaub <91491081+aleksandr-shtaub@users.noreply.github.com>
2024-07-16 18:25:13 +0300

    Add firebase configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/',
-  // apiBaseUrl: 'http://192.168.1.101:3000/',
+  apiBaseUrl: 'http://localhost:3000/', // TODO: remove the line
+  firebase: {
+  }
 };
