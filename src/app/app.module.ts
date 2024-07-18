afa2a2dbd719d2c1a52311d41c10d6af48d3d65f
Aleksandr Shtaub <91491081+aleksandr-shtaub@users.noreply.github.com>
2024-07-18 21:09:00 +0300

    Add firebase emulators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterLink, RouterModule } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
@@ -23,8 +23,24 @@ import { SpinnerComponent } from './common/components/spinner/spinner.component'
     NavigationComponent,
     HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(firestore, environment.emulators.baseUrl, environment.emulators.firestorePort);
+      }
+
+      return firestore;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, `http://${environment.emulators.baseUrl}:${environment.emulators.authPort}`);
+      }
+
+      return auth;
+    }),
   ],
   providers: [RouterLink],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,18 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/', // TODO: remove the line
+  apiBaseUrl: 'http://localhost:3000/',
+  useEmulators: true,
+  emulators: {
+    baseUrl: 'localhost',
+    firestorePort: 8080,
+    authPort: 9099,
+  },
   firebase: {
-  }
+    apiKey: 'apiKey',
+    authDomain: 'task-management-tc.firebaseapp.com',
+    projectId: 'task-management-tc',
+    storageBucket: 'task-management-tc.appspot.com',
+    messagingSenderId: '1096952800242',
+    appId: 'appId',
+  },
 };
